0d4f969255a3955804f1d615ebc1cbb235b816d1
vilnout <vilnout0@gmail.com>
2026-03-24 16:06:24 +0530

    feat: add resizable request and reponse panels

---
+++ src/components/layout/WorkspacePanel.tsx
@@ -0,0 +1,28 @@
+import { RequestPanel } from "@/components/request/RequestPanel";
+import { ResponsePanel } from "@/components/response/ResponsePanel";
+import { Group, Panel, Separator } from "react-resizable-panels";
+
+export const WorkspacePanel = () => {
+  return (
+    <Group orientation="vertical">
+      <Panel
+        defaultSize="45%"
+        minSize="25%"
+        className="flex flex-col overflow-hidden"
+      >
+        <RequestPanel />
+      </Panel>
+
+      <Separator className="group bg-border-default hover:bg-accent active:bg-accent flex h-1.5 shrink-0 items-center justify-center transition-colors">
+        <div className="bg-border-subtle group-hover:bg-accent-hover h-0.5 w-8 rounded-full transition-colors group-active:bg-white" />
+      </Separator>
+      <Panel
+        defaultSize="55%"
+        minSize="20%"
+        className="flex flex-col overflow-hidden"
+      >
+        <ResponsePanel />
+      </Panel>
+    </Group>
+  );
+};
