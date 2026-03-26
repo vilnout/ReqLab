e20a3efd5beefcc7f37150dad2310f6d3b6d3869
vilnout <vilnout0@gmail.com>
2026-03-26 10:54:25 +0530

    feat: add sidebar for mobile layouts

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,16 +1,31 @@
+import { MobileDrawer } from "@/components/layout/MobileDrawer/MobileDrawer";
+import { MobileHamburger } from "@/components/layout/MobileDrawer/MobileHeader";
 import { Sidebar } from "@/components/layout/sidebar";
+import { useState } from "react";
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export const AppLayout = ({ children }: AppLayoutProps) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <div className="flex flex-1 overflow-hidden">
-      <Sidebar />
-      <main className="bg-surface-base flex flex-1 flex-col overflow-hidden">
-        {children}
-      </main>
-    </div>
+    <>
+      <MobileDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
+
+      <div className="flex flex-1 overflow-hidden">
+        <div className="hidden md:flex">
+          <Sidebar />
+        </div>
+        <main className="bg-surface-base flex min-w-0 flex-1 flex-col overflow-hidden">
+          <MobileHamburger handleClick={() => setDrawerOpen(true)} />
+          {children}
+        </main>
+      </div>
+    </>
+    //   <main className="bg-surface-base flex flex-1 flex-col overflow-hidden">
+    //     {children}
+    //   </main>
+    // </div>
   );
 };
